db8819e65802ea5f6761426ac8e0f558546326d3
Marco Moura <marcomoura27052009@gmail.com>
2026-04-25 08:24:22 -0300

    Enhance contact section styles and update form handling for Cliente model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Projeto, Tecnologia, Skill
+from .models import Projeto, Tecnologia, Skill, Cliente
 # Register your models here.
 
 
 admin.site.register(Projeto)
 admin.site.register(Tecnologia)
 admin.site.register(Skill)
+admin.site.register(Cliente)
--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,21 @@ from django import forms
 from .models import Cliente
 
 
-class Form(forms.Form):
-    model = Cliente
+class Form(forms.ModelForm):
+    class Meta:
+        model = Cliente
 
-    fields = ['nome', 'email', 'mensagem']
+        fields = '__all__'
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'campo-nome',
+                'placeholder': 'Seu nome'}),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'campo-email',
+                'placeholder': 'seu@email.com'}),
+
+            'mensagem': forms.Textarea(attrs={
+                'class': 'campo-mensagem',
+                'placeholder': 'Descreva o que você tem em mente...'})
+        }
