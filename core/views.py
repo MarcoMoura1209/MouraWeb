c005f0c6b447ad3e6338493e2f14982db8bbd075
Marco Moura <marcomoura27052009@gmail.com>
2026-06-09 18:43:41 -0300

    Adicao do sitemap.xml e implementacao da funcao para geracao dinamica do sitemap

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 import logging
 from django.shortcuts import render, redirect
 from django_ratelimit.decorators import ratelimit
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 from .models import Projeto, Skill
 from .forms import Form
 from honeypot.decorators import check_honeypot
@@ -27,3 +29,26 @@ def home(request):
         'skills': skills,
         'form': form,
     },)
+
+
+@require_http_methods(["GET"])
+def sitemap(request):
+    """Gera e serve o sitemap.xml dinamicamente"""
+    sitemap_content = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+
+    <url>
+        <loc>https://mouraweb.com.br/</loc>
+        <lastmod>2026-06-09</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+        <xhtml:link
+            rel="alternate"
+            hreflang="pt-BR"
+            href="https://mouraweb.com.br/"/>
+    </url>
+
+</urlset>'''
+
+    return HttpResponse(sitemap_content, content_type='application/xml')
